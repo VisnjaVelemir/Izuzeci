519ae60ae614a2b24062d843892c399ca5c0013f
Visnja <visnjax@gmail.com>
2019-11-29 21:38:49 +0100

    Exception

---
+++ Izuzeci/src/izuzeci/GlavnaIzuzeci.java
@@ -0,0 +1,49 @@
+package izuzeci;
+
+public class GlavnaIzuzeci {
+
+	public static void main(String[] args) {
+		
+		
+		Metode m = new Metode ();
+		System.out.println(m.podeli (10,2));
+		
+		System.out.println(m.podeli(10, 2));
+		int a = 10;
+		int b = 2;
+		
+		try {
+			System.out.println(a/b);
+			System.out.println("Radi!!!!");
+			
+		} catch (Exception e) {
+			
+			System.out.println("NECEEEEEEEE!!!!!");
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
---
+++ Izuzeci/src/izuzeci/Metode.java
@@ -0,0 +1,61 @@
+package izuzeci;
+
+public class Metode {
+
+	
+	public double podeli (int a, int b) {
+		return b;
+	}
+		
+		
+		
+		// try je isto sto i for, a catch je isto sto i else...ovo se koristi za exceptionse
+		
+	/*	 try {
+		
+	}
+	
+		 catch (exceptione)*/
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
